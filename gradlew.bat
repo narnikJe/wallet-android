cd990972b0927c09976f4c1dd1017ca841f5e5c7
narnikJe <137431510+narnikJe@users.noreply.github.com>
2023-10-19 12:57:46 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -28,3 +28,5 @@ echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PA
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
+
+goto fail
